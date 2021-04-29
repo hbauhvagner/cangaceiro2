8968424048d826827983c1e6c0890c30fa4df8ef
hbauhvagner <hbauhvagner@gmail.com>
2021-04-29 01:18:45 -0300

    Revisando Closure

--- public/app/app.js
+++ public/app/app.js
@@ -1,14 +1,17 @@
 import { handleStatus, log } from './utils/promise-helpers.js';
 
+import './utils/array-helpers.js';
+
+const sumItems = code => notas => notas
+    .$flatMap(nota => nota.itens)
+    .filter(item => item.codigo == code)
+    .reduce((total, item) => total + item.valor, 0);
+
 document
 .querySelector('#myButton')
 .onclick = () => 
     fetch('http://localhost:3000/notas')
         .then(handleStatus)
-        .then(notas => notas.reduce((array, nota) => array.concat(nota.itens), []))
-        .then(log)
-        .then(itens => itens.filter(item => item.codigo == '2143'))
-        .then(log)
-        .then(itens => itens.reduce((total, item) => total + item.valor, 0))
+        .then(sumItems('2143'))
         .then(log)
         .catch(log);
